cb6a5a0d91a57e65b6b6ab741401b0f1bca2c64f
Victor Falconi <v.falconi.m@gmail.com>
2023-03-02 07:47:12 -0500

    Database update

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const sequelize = new Sequelize(`postgres://videogames_database_user:jvfutK2l1r5DOO7hitZel9e8yGeTEIiy@dpg-cg09fi64dad93e10u9f0-a.oregon-postgres.render.com/videogames_database`, {
+const sequelize = new Sequelize(`postgres://videogames_database_user:jvfutK2l1r5DOO7hitZel9e8yGeTEIiy@dpg-cg09fi64dad93e10u9f0-a.oregon-postgres.render.com:5432/videogames_database`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
